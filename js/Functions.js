22d8266adb9cd96b63a35191b0082ebccc3b8fde
Dani15 <Chimdichan@gmail.com>
2023-08-22 22:37:02 +0100

    My second commit

---
+++ js/Functions.js
@@ -0,0 +1,60 @@
+// This function is to check when the game is won
+
+function checkIfGameIsWon (first, second, third) {
+    if (display.textContent.includes("won the game")) {
+        return;
+    }
+    if (
+    buttons[first].textContent === "X" &&
+    buttons[second].textContent === "X" &&
+    buttons[third].textContent === "X" ) {
+        disableButtonWhenGameIsWon("Player 1");
+        return;
+    } else if (
+    buttons[first].textContent === "O" &&
+    buttons[second].textContent === "O" &&
+    buttons[third].textContent === "O" 
+    ) {
+        disableButtonWhenGameIsWon("Player 2");
+        return;
+    } else { 
+    }
+        checkDraw();
+}
+
+function checkStatus () {
+    // check if the game has been won horizontally
+    checkIfGameIsWon(0, 1, 2);
+    checkIfGameIsWon(3, 4, 5);
+    checkIfGameIsWon(6, 7, 8);
+
+    // check if the game has been won vertically
+    checkIfGameIsWon(0, 3, 6);
+    checkIfGameIsWon(1, 4, 7);
+    checkIfGameIsWon(2, 5, 8);
+
+    // check if the game has been won diagonally
+    checkIfGameIsWon(0, 4, 8);
+    checkIfGameIsWon(2, 4, 6);
+}
+
+function disableButtonWhenGameIsWon(player) {
+    for (let button of buttons) {
+        if (button.textContent === "") {
+            button.toggleAttribute("disabled")
+        }
+    }
+    display.textContent = player + "won the game"
+}
+
+function checkDraw() {
+    for (let button of buttons) {
+        if (button.textContent === ""){
+            return;
+        }
+    }
+    display.textContent = "It is a Draw Game";
+}
+
+
+
